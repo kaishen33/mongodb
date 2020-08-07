893ff03668c80d19a0e4aac9226de307a0ef8bb5
KaiWei Shen <keshen@ucdavis.edu>
2020-08-06 18:16:28 -0700

    finished mongo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config()
 const PORT = 3000;
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/workout", {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 }).then(() => {
